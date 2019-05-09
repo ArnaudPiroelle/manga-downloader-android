103f3eaee89a5c1e82a22debc327300447e20bcf
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2019-05-09 20:20:00 +0200

    Remove withContext to use room coroutines dispatcher

--- interactors/src/main/java/com/arnaudpiroelle/manga/interactors/GetProvidersInteractor.kt
+++ interactors/src/main/java/com/arnaudpiroelle/manga/interactors/GetProvidersInteractor.kt
@@ -1,11 +1,9 @@
 package com.arnaudpiroelle.manga.interactors
 
 import com.arnaudpiroelle.manga.api.core.provider.ProviderRegistry
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 
-class GetProvidersInteractor(private val providerRegistry: ProviderRegistry, private val dispatcher: CoroutineDispatcher) {
-    suspend operator fun invoke() = withContext(dispatcher) {
-        providerRegistry.list().values.toList()
+class GetProvidersInteractor(private val providerRegistry: ProviderRegistry) {
+    operator fun invoke(): List<ProviderRegistry.Provider> {
+        return providerRegistry.list().values.toList()
     }
 }
