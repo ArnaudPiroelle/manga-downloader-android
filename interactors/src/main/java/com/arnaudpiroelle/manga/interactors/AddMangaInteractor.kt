b746f9b097918ae99345753660e02a1463caca40
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2019-05-09 14:42:29 +0200

    Add history entry when a manga is added

--- interactors/src/main/java/com/arnaudpiroelle/manga/interactors/AddMangaInteractor.kt
+++ interactors/src/main/java/com/arnaudpiroelle/manga/interactors/AddMangaInteractor.kt
@@ -1,19 +1,29 @@
 package com.arnaudpiroelle.manga.interactors
 
 import com.arnaudpiroelle.manga.api.model.Manga
+import com.arnaudpiroelle.manga.data.dao.HistoryDao
 import com.arnaudpiroelle.manga.data.dao.MangaDao
+import com.arnaudpiroelle.manga.data.model.History
 import com.arnaudpiroelle.manga.data.model.Manga.Status.ADDED
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
-class AddMangaInteractor(private val mangaDao: MangaDao, private val dispatcher: CoroutineDispatcher) {
+class AddMangaInteractor(private val mangaDao: MangaDao, private val historyDao: HistoryDao, private val dispatcher: CoroutineDispatcher) {
     suspend operator fun invoke(manga: Manga) = withContext(dispatcher) {
-        mangaDao.insert(com.arnaudpiroelle.manga.data.model.Manga(
+        val mangaId = mangaDao.insert(com.arnaudpiroelle.manga.data.model.Manga(
                 name = manga.name,
                 alias = manga.alias,
                 provider = manga.provider,
                 thumbnail = "",
                 status = ADDED)
+
         )
+
+        historyDao.insert(History(
+                label = "${manga.name} (${manga.provider})",
+                sublabel = "Added"
+        ))
+
+        mangaId
     }
 }
